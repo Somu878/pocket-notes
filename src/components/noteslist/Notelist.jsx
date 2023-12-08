759e3df873c25f39af68347f2a0d241e65e0c88e
somu878 <somumydhili19@gmail.com>
2023-12-08 10:11:44 +0530

    added modal for adding group

--- src/components/noteslist/Notelist.jsx
+++ src/components/noteslist/Notelist.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styles from './Notelist.module.css'
 import List from './List'
+import Modal from 'react-modal';
+const customStyles = {
+  content: {
+   width: '570px',
+   height:'235px',
+   marginTop:'200px',
+   marginLeft:'400px',
+   paddingLeft:'40px',
+   paddingTop:'0px'
+  }
+};
 function Notelist() {
-    const col = 'pink'
-    const name = 'hello world'
+  const [modalIsOpen, setIsOpen] =useState(false);
+  const col = 'pink'
+  const name = 'hello world'
   return (
     <div style={{width:'30vw',height:'100vh',display:'flex',flexDirection:'column'}}>
         <p className={styles.title}>Pocket Notes</p>
         <List  grpColour={col} grpName={name} />
-        <button className={styles.addBtn}>+</button>
+        <Modal style={customStyles} isOpen={modalIsOpen}>
+        <p style={{fontFamily:'var(--roboto)',fontSize:'27px',fontWeight:'500'}}>Create New Group</p>
+        <div style={{display:'flex'}}>
+          <p className={styles.inputlabel}>Group Name</p>
+          <input type="text" className={styles.grpInput} placeholder='Enter Group Name' />
+        </div>
+        </Modal>
+        <button className={styles.addBtn} onClick={()=>setIsOpen(true)}>+</button>
     </div>
   )
 }
-
 export default Notelist
+
+
