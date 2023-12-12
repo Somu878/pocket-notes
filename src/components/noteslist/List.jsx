0683f73bda09897055b72e5a4234092c0a156cea
somu878 <somumydhili19@gmail.com>
2023-12-12 12:36:57 +0530

    updated media queries #2

--- src/components/noteslist/List.jsx
+++ src/components/noteslist/List.jsx
@@ -1,26 +1,19 @@
-import React, { useState } from "react";
-import styles from "./List.module.css";
-import { useNavigate, Link } from "react-router-dom";
-import Getinitials from "../../helper functions/Getinitials";
-function List({ grpColour, grpName, grpId, selectedGroup, setselectedGroup }) {
-  const navigate = useNavigate();
-  const InitalsofGrpName = Getinitials(grpName);
+import React, { useState } from 'react'
+import styles from './List.module.css'
+import {useNavigate,Link } from 'react-router-dom'
+import Getinitials from '../../helper functions/Getinitials'
+function List({grpColour,grpName,grpId,selectedGroup,setselectedGroup}) {
+  const navigate =useNavigate()
+    const InitalsofGrpName= Getinitials(grpName)
   return (
-    <div
-      className={`${styles.list} ${
-        selectedGroup === grpId ? styles.selected : ""
-      }`}
-      onClick={() => {
-        setselectedGroup(grpId);
-        navigate(`/content/${grpId}`);
-      }}
-    >
-      <div className={styles.grpicon} style={{ background: grpColour }}>
-        {InitalsofGrpName}
-      </div>
-      <div className={styles.listgrpname}>{grpName}</div>
+    <div className={`${styles.list} ${selectedGroup === grpId ? styles.selected : ''}`}  onClick={()=>{
+      setselectedGroup(grpId)
+      navigate(`/content/${grpId}`)
+    }}>
+       <div className={styles.grpicon} style={{background:grpColour}}>{InitalsofGrpName}</div>
+        <div className={styles.listgrpname}>{grpName}</div>
     </div>
-  );
+  )
 }
 
-export default List;
+export default List
