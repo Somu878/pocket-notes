5ac31d1810ba9726e1c37649e3fa3fd6911b68d1
somu878 <somumydhili19@gmail.com>
2023-12-13 20:24:54 +0530

    Updated Getinitials.js

--- src/helper functions/Getinitials.js
+++ src/helper functions/Getinitials.js
@@ -1,14 +1,13 @@
-const Getinitials=(grpname)=>{
-    const words = grpname.trim().split(' ')
-    if (words.length>=2){
-        const firstInitial = words[0][0].toUpperCase()
-        const secondInitial = words[1][0].toUpperCase()
-        return `${firstInitial}${secondInitial}`
-    }
-    else if(words.length<2){
-        return words[0].slice(0,2).toUpperCase()
-    }
-    return 'NA'
-}
+const Getinitials = (grpname) => {
+  const words = grpname.trim().split(" ");
+  if (words.length >= 2) {
+    const firstInitial = words[0][0].toUpperCase();
+    const secondInitial = words[1][0].toUpperCase();
+    return `${firstInitial}${secondInitial}`;
+  } else if (words.length < 2) {
+    return words[0].slice(0, 1).toUpperCase();
+  }
+  return "NA";
+};
 
-export default Getinitials
+export default Getinitials;
